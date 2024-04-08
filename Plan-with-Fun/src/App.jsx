1b6acf40662c21dc8a5d21a6c008fc9949e1f458
Rishi Kumar Singh <rishisingh43210@gmail.com>
2024-04-08 22:11:51 +0530

    Plan with Fun

---
+++ Plan-with-Fun/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cards from './components/Cards'
+const App = () => {
+  return (
+    <>
+    <h1 className= ' ml-[34rem] mr-[34rem] mt-5 p-3 text-5xl text-center border-[6px] border-solid border-stone-600 rounded-md'>Plan With Fun</h1>
+    <Cards/>
+    </>
+  )
+}
+
+export default App
